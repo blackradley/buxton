7de2413f627096f16adb51dba55968b343cee4c6
Joe J Collins <joejcollins@gmail.com>
2007-03-30 12:39:32 +0000

    Initial import.

---
+++ app/controllers/organisation_controller.rb
@@ -0,0 +1,53 @@
+class OrganisationController < ApplicationController
+  layout "default"
+  
+  def index
+    list
+    render :action => 'list'
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    @organisation_pages, @organisations = paginate :organisations, :per_page => 10
+  end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(params[:organisation])
+    if @organisation.save
+      flash[:notice] = 'Organisation was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def update
+    @organisation = Organisation.find(params[:id])
+    if @organisation.update_attributes(params[:organisation])
+      flash[:notice] = 'Organisation was successfully updated.'
+      redirect_to :action => 'show', :id => @organisation
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    Organisation.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+end
---
+++ db/migrate/001_create_organisations.rb
@@ -0,0 +1,18 @@
+class CreateOrganisations < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Schema.define(:version => 0) do
+      create_table "organisations", :force => true do |t|
+        t.column "key",        :string,    :limit => 36,  :default => "", :null => false
+        t.column "email",      :string,    :limit => 256, :default => "", :null => false
+        t.column "name",       :string,    :limit => 256, :default => "", :null => false
+        t.column "style",      :string,    :limit => 256, :default => "", :null => false
+        t.column "created_on", :timestamp
+        t.column "updated_on", :timestamp
+      end
+    end
+  end
+
+  def self.down
+    drop_table :organisations
+  end
+end
