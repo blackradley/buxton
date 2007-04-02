ccb091a02c7da1acae3bd0f61dfc71283fc54a39
Joe Collins <joe@bagbatch.co.uk>
2007-04-02 19:51:56 +0000

    Allow nulls in relevance fields

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,8 +1,8 @@
 class Email < ActionMailer::Base
-  def new_key(function, organisation)
+  def new_key(function)
     # Email header info MUST be added here
     recipients function.email
-    from organisation.email
+    from function.organisation.email
     subject 'Link to EINA'
     # Email body substitutions go here
     body :subdomain => organisation.style, :key => function.key
--- db/migrate/001_create_organisations.rb
+++ db/migrate/001_create_organisations.rb
@@ -1,15 +1,15 @@
 class CreateOrganisations < ActiveRecord::Migration
   def self.up
-    #ActiveRecord::Schema.define(:version => 0) do
+    ActiveRecord::Schema.define do
       create_table "organisations", :force => true do |t|
-        t.column "key",        :string,    :limit => 36,  :default => "", :null => false
-        t.column "email",      :string,    :limit => 256, :default => "", :null => false
-        t.column "name",       :string,    :limit => 256, :default => "", :null => false
-        t.column "style",      :string,    :limit => 256, :default => "", :null => false
+        t.column "key",        :string,    :limit => 128,  :null => true
+        t.column "email",      :string,    :limit => 256,  :null => true
+        t.column "name",       :string,    :limit => 256,  :null => true
+        t.column "style",      :string,    :limit => 256,  :null => true
         t.column "created_on", :timestamp
         t.column "updated_on", :timestamp
       end
-   # end
+    end
   end
 
   def self.down
