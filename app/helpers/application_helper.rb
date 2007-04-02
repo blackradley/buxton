95e4581213f79b5250d428f1471f6db5e8b1b130
Joe Collins <joe@bagbatch.co.uk>
2007-04-02 07:20:01 +0000

    Get a UUID() from the database

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
   def ApplicationHelper.subdomain(request)
     return request.subdomains(0).first
   end
+  
+  def ApplicationHelper.UUID
+    return ActiveRecord::Base.connection.select_one('select UUID()')['UUID()']
+  end
+  
 end
