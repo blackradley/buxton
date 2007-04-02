5b0778cb9c3de8e4756390b76689c9c7064d98a6
Joe J Collins <joejcollins@gmail.com>
2007-04-02 19:51:56 +0000

    Allow nulls in relevance fields

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,11 +1,11 @@
 class Function < ActiveRecord::Base
   belongs_to :organisation
   def state
-    read_attribute(:relevance01) +
-    read_attribute(:relevance02) +
-    read_attribute(:relevance03) +
-    read_attribute(:relevance04) +
-    read_attribute(:relevance05) +
-    read_attribute(:relevance06)
+    read_attribute(:relevance01).nil? ? 0 : 1 +
+    read_attribute(:relevance02).nil? ? 0 : 1 +
+    read_attribute(:relevance03).nil? ? 0 : 1 +
+    read_attribute(:relevance04).nil? ? 0 : 1 +
+    read_attribute(:relevance05).nil? ? 0 : 1 +
+    read_attribute(:relevance06).nil? ? 0 : 1
   end
 end
--- db/migrate/002_create_functions.rb
+++ db/migrate/002_create_functions.rb
@@ -1,22 +1,24 @@
 class CreateFunctions < ActiveRecord::Migration
   def self.up
-    create_table "functions", :force => true do |t|
-      t.column "key",             :string,    :limit => 36,  :default => "", :null => false
-      t.column "email",           :string,    :limit => 256, :default => "", :null => false
-      t.column "name",            :string,    :limit => 256, :default => "", :null => false
-      t.column "organisation_id", :integer,   :limit => 10,                  :null => false
-      t.column "relevance01",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance02",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance03",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance04",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance05",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance06",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance07",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance08",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance09",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "relevance10",     :integer,   :limit => 10,  :default => 1, :null => false
-      t.column "created_on",      :timestamp
-      t.column "updated_on",      :timestamp
+    ActiveRecord::Schema.define do
+      create_table "functions", :force => true do |t|
+        t.column "key",             :string,    :limit => 128, :null => true
+        t.column "email",           :string,    :limit => 256, :null => true
+        t.column "name",            :string,    :limit => 256, :null => true
+        t.column "organisation_id", :integer,   :limit => 2,   :null => true
+        t.column "relevance01",     :integer,   :limit => 2,   :null => true
+        t.column "relevance02",     :integer,   :limit => 2,   :null => true
+        t.column "relevance03",     :integer,   :limit => 2,   :null => true
+        t.column "relevance04",     :integer,   :limit => 2,   :null => true
+        t.column "relevance05",     :integer,   :limit => 2,   :null => true
+        t.column "relevance06",     :integer,   :limit => 2,   :null => true
+        t.column "relevance07",     :integer,   :limit => 2,   :null => true
+        t.column "relevance08",     :integer,   :limit => 2,   :null => true
+        t.column "relevance09",     :integer,   :limit => 2,   :null => true
+        t.column "relevance10",     :integer,   :limit => 2,   :null => true
+        t.column "created_on",      :timestamp
+        t.column "updated_on",      :timestamp
+      end
     end
   end
 
