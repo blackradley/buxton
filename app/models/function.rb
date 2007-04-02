941057d28f4c00fc79ba0a051db7ebfe7e43f3d1
Joe J Collins <joejcollins@gmail.com>
2007-04-02 07:22:09 +0000

    Summary of fields for functions.

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,3 +1,13 @@
 class Function < ActiveRecord::Base
   belongs_to :organisation
+  
+  def state
+    read_attribute(:relevance01) +
+    read_attribute(:relevance02) +
+    read_attribute(:relevance03) +
+    read_attribute(:relevance04) +
+    read_attribute(:relevance05) +
+    read_attribute(:relevance06)
+  end
+    
 end
