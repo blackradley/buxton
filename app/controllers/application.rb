f3a6a772f1a18f213c171b93c536b0da8f6d4ffd
Joe J Collins <joejcollins@gmail.com>
2007-04-03 17:51:06 +0000

    Added a look ups table for the options.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,10 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-    #before_filter :authenticate
-  #before_filter :authorize
- 
- 
+  before_filter :authenticate
+  
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_buxton_session_id'
   
@@ -15,29 +13,19 @@ class ApplicationController < ActionController::Base
     :date_time12 => "%d %b %Y %I:%M%p",
     :date_time24 => "%d %b %Y %H:%M"
   )
-  
-  protected
-    def authenticate
-      unless @session["person"]
-        @session["return_to"] = @request.request_uri
-        redirect_to :controller => "login" 
-        return false
-      end
-    end
 
   protected
-    # Override in controller classes that should require authentication
-    def secure?
-      false
-    end
+  # Override in controller classes that should require authentication
+  def secure?
+    false
+  end
 
   private
-    def authorize
-      if secure? && session["person"].nil?
-        session["return_to"] = request.request_uri
-        redirect_to :controller => "auth" 
-        return false
-      end
-    end
-  
+  def authenticate
+#    if secure? && session["person"].nil?
+#      session["return_to"] = request.request_uri
+#      redirect_to :controller => "auth" 
+#      return false
+#    end
+  end
 end
