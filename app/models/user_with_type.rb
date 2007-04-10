0b7a33945b105ad0b958baae370e8ac3cb70bfad
Joe Collins <joe@bagbatch.co.uk>
2007-04-10 17:55:43 +0000

    Commit with svn keywords.

--- app/models/user_with_type.rb
+++ app/models/user_with_type.rb
@@ -1,10 +1,11 @@
 #  
-# * $URL: http://svn3.cvsdude.com/BlackRadley/buxton/trunk/app/models/user.rb $
-# * $Rev: 36 $
-# * $Author: BlackRadleyJoe $
-# * $Date: 2007-04-10 16:16:19 +0100 (Tue, 10 Apr 2007) $
+# * $URL$
+# * $Rev$
+# * $Author$
+# * $Date$
 # 
 class UserWithType < ActiveRecord::Base
   has_one :organisation
   has_one :function
 end
+
