95a79f4c1622f5a0009b886340238190f9cef028
Joe Collins <joe@bagbatch.co.uk>
2007-04-10 05:05:59 +0000

    Keyword expansion test.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,19 @@
-# Methods added to this helper will be available to all templates in the application.
+# Methods added to this helper will be available to all 
+# templates in the application.
+# 
+# $Id: foo.bar 123 2006-02-02 08:40:12Z agorf $
 module ApplicationHelper
 
-  def ApplicationHelper.subdomain(request)
-    return request.subdomains(0).first
+  # I could use request.subdomains(0).first but it throws
+  # a wobbly if in the development environment because
+  # there is no subdomain on http://localhost:3000/
+  def subdomain(request)
+    return request.host().split(/\s*\.\s*/)[0] 
   end
   
-  def ApplicationHelper.newUUID
+  # Thers is no built in method for creating a GUID in Ruby
+  # so the UUID in MySql is called instead.
+  def newUUID
     return ActiveRecord::Base.connection.select_one('select UUID()')['UUID()']
   end
   
