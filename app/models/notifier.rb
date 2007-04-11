63501812ca0b0b2ca48a87211add006ab9256f08
Joe Collins <joe@bagbatch.co.uk>
2007-04-11 12:20:57 +0000

    Email with key.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -6,12 +6,12 @@
 # 
 class Notifier < ActionMailer::Base
 
-  def new_key(sent_at = Time.now)
-    @subject    = 'Notifier#new_key'
-    @body       = {}
-    @recipients = 'drbollins@hotmail.com'
-    @from       = 'equality@blackradley.com'
-    @sent_on    = sent_at
-    @headers    = {}
+  def new_key(user)
+    @subject      = 'New Key'
+    @body["user"] = user
+    @recipients   = 'drbollins@hotmail.com'
+    @from         = 'equality@blackradley.com'
+    @sent_on      = Time.now
+    @headers      = {}
   end
 end
