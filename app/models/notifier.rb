a91d8aa28ca9d99a615e4f7d5b03aa044219a9b0
Joe Collins <joe@bagbatch.co.uk>
2007-04-11 12:46:35 +0000

    Three types of email for the three types of user, sadly it doesn't work.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -6,12 +6,34 @@
 # 
 class Notifier < ActionMailer::Base
 
-  def new_key(user)
-    @subject      = 'New Key'
+  FROM = 'equality_support@blackradley.com'
+  
+  def administration_key(user)
+    @subject      = 'New Administration Key'
     @body["user"] = user
-    @recipients   = 'drbollins@hotmail.com'
-    @from         = 'equality@blackradley.com'
+    @recipients   = user.email
+    @from         = FROM
     @sent_on      = Time.now
     @headers      = {}
   end
+  
+  def organisation_key(user, organisation)
+    @subject      = 'New Organisation Key'
+    @body["user"] = user
+    @recipients   = user.email
+    @from         = FROM
+    @sent_on      = Time.now
+    @headers      = {}
+  end
+  
+  def function_key(user, function)
+    @subject      = 'New Function Key'
+    @body["user"] = user
+    @recipients   = user.email
+    @from         = FROM
+    @sent_on      = Time.now
+    @headers      = {}
+  end
+  
+  
 end
