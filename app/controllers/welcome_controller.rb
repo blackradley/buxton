0bc2b30e6afb225dff3f20aecc6a81dfe0828478
Joe Collins <joe@bagbatch.co.uk>
2007-04-12 00:18:36 +0000

    Trying to send an email from welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,9 +9,31 @@ class WelcomeController < ApplicationController
   
   def index
   end
-  
+#
+# Find the user and then send them a new key
+#
   def new_key
-    @function = Function.find_by_email(params[:email])
-    Email.deliver_new_key(@function)
+    @users = User.find_all_by_email(params[:email])
+    if @users.empty? 
+      flash[:notice] = 'Unknown Email'
+    else
+      for user in @users
+        case user.user_type
+          when User::FUNCTIONAL
+            
+          when User::ORGANISATIONAL
+            
+          when User::ADMINISTRATIVE
+            @user.passkey = User.new_UUID
+            @user.reminded_on = Time.now
+            @user.save
+            email = Notifier.create_administration_key(@user)
+            Notifier.deliver(email)
+            flash[:notice] = 'New key sent to ' + @user.email
+        end
+      end
+    end
+    redirect_to :action => 'index'
   end
+  
 end
