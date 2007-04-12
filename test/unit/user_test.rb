cfa4ad807eb41a7820c73ce42282afcb0ca66682
Joe Collins <joe@bagbatch.co.uk>
2007-04-12 12:40:23 +0000

    Tests run, ok the email test fail, but they run.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,10 +1,20 @@
+#  
+# * $URL$
+# * $Rev$
+# * $Author$
+# * $Date$
+# 
 require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
   fixtures :users
 
+  def setup
+    @user = users(:three)
+  end
+
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_first_user
+    assert_equal @user.email, 'drbollins@hotmail.com'
   end
 end
