a934003797806fe451af64462ca0ff8451526606
Joe J Collins <joejcollins@gmail.com>
2007-04-18 11:54:52 +0000

    Addtions and changes to cope with deployment on to www.34sp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,9 @@ config.cache_classes = true
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
+# Not true, reversed by Joe Collins
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
