0e7a9b10b87cbf2214070fb572069afaec9406fe
Joe J Collins <joejcollins@gmail.com>
2007-04-19 18:44:01 +0000

    Mangement of impact groups.

--- app/models/impact_group.rb
+++ app/models/impact_group.rb
@@ -1,2 +1,12 @@
+#  
+# * $URL$
+# * $Rev$
+# * $Author$
+# * $Date$
+# 
 class ImpactGroup < ActiveRecord::Base
+  belongs_to :organisation
+  validates_presence_of :name
+  validates_numericality_of :display_order
+  
 end
