b2e999e6c1cc9157300dde6f23baf82efef2f603
Joe Collins <joe@bagbatch.co.uk>
2007-04-24 18:16:59 +0000

    Linking table.

--- app/models/function_impact_group.rb
+++ app/models/function_impact_group.rb
@@ -1,8 +1,8 @@
 #  
-# $URL: http://svn3.cvsdude.com/BlackRadley/buxton/trunk/app/models/impact_group.rb $
-# $Rev: 89 $
-# $Author: BlackRadleyJoe $
-# $Date: 2007-04-22 21:56:17 +0100 (Sun, 22 Apr 2007) $
+# $URL$
+# $Rev$
+# $Author$
+# $Date$
 #
 # Copyright (c) 2007 Black Radley Limited. All rights reserved. 
 #
