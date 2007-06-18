cb2ad83fb07ac7bcfb445c2a2f987ee0f8e00acd
Joe Collins <joe@bagbatch.co.uk>
2007-06-18 14:09:22 +0000

    The impact groups are the same for all organisations so the facilities for editing them have been removed.

--- app/controllers/organisation_controller.rb
+++ app/controllers/organisation_controller.rb
@@ -16,21 +16,26 @@ class OrganisationController < ApplicationController
     list
     render :action => 'list'
   end
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+#
+# GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+# 
   verify :method => :post, :only => [ :destroy, :create, :update ],
          :redirect_to => { :action => :list }
-
+#
+# List the organisation for the administrative User.
+#
   def list
     @organisation_pages, @organisations = paginate :organisations, :per_page => 10
   end
-
+#
+# Show a view of an individual Organisation
+#
   def show
     @organisation = Organisation.find(params[:id])
     @user = @organisation.user
   end
 #
-# Create a new organisation and a new associated user
+# Create a new Organisation and a new associated User
 #
   def new
     @organisation = Organisation.new
