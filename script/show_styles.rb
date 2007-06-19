628bce14841938c357dbf2b33e573261ca4838bf
Joe Collins <joe@bagbatch.co.uk>
2007-06-19 10:55:37 +0000

    Scripted automation of IE so we can review the style sheets.

--- script/show_styles.rb
+++ script/show_styles.rb
@@ -21,16 +21,37 @@ end
 
 DEMO_HOST = 'http://www.localhost:3000/'
 ORGANISATION_HOST = 'http://birmingham.localhost:3000/'
-
 ie = WIN32OLE.new('InternetExplorer.Application')
 ie.Visible = true
+ie.height = 600
+ie.width = 800
+
+puts 'Demonstration Create Page - press return'
 ie.Navigate(DEMO_HOST)
 wait_for_IE(ie) 
+input = gets
 
-ie = WIN32OLE.new('InternetExplorer.Application')
-ie.Visible = true
-ie.Navigate(ORGANISATION_HOST)
-wait_for_IE(ie) 
+puts 'Demonstration Create with Bogus email - press return'
+ie.Document.All.email.Value = 'not an email address'
+ie.Document.All.commit.click
+input = gets
+
+puts 'CEPT Organisation Control Page - press return'
+ie.Document.All.email.Value = 'demo@localhost.com'
+ie.Document.All.commit.click
+input = gets
+
+puts 'CEPT Section 1 Page - press return'
+ie.Document.All.list1.click
+input = gets
+
+puts 'CEPT Section 1 view - press return'
+ie.Document.All.view(0).click
+input = gets
+
+puts 'CEPT Section 1 amend - press return'
+ie.Document.All.amend.click
+input = gets
 
 ie.quit
 
