a39bfcb89e96f45525de14fa78f9b018deaf3d04
Karl Doody <karl@27stars.co.uk>
2007-09-13 09:57:17 +0000

    Question text/type storage improvement (ready for stats). Question display improvement (helpers / formbuilder - cleaner code / easier to upgrade/modify/maintain)

---
+++ app/helpers/question_builder.rb
@@ -0,0 +1,28 @@
+class QuestionBuilder < ActionView::Helpers::FormBuilder    
+
+  # Generates all the HTML needed for a form question
+  def question(method, options={})
+    # Get the label text for this question
+    label = $questions[method][0]
+
+    # Get the answer options for this question and make an appropriate input field
+    input_field = case $questions[method][1]
+    when :existing_proposed
+      select method, LookUp.existing_proposed.collect {|l| [ l.name, l.value ] }
+    when :impact_amount
+      select method, LookUp.impact_amount.collect {|l| [ l.name, l.value ] }
+    when :impact_level
+      select method, LookUp.impact_level.collect {|l| [ l.name, l.value ] }
+    when :rating
+      '1-5'
+    when :yes_no_notsure
+      'yes no notsure'
+    when :text
+      text_area method 
+    end
+    
+    # Show our formatted question!
+    %Q[<p><label for="#{object_name.to_s}_#{method.to_s}">#{label}</label>#{input_field}</p>]
+  end
+  
+end
