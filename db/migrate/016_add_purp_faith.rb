12038ccbe231a2967468deeb8fa88b1e599f8dd6
Joe Pym <joe@27stars.co.uk>
2007-10-09 15:21:29 +0000

    Added extra questions to the migration

---
+++ db/migrate/016_add_purp_faith.rb
@@ -0,0 +1,11 @@
+class AddPurpFaith < ActiveRecord::Migration
+  def self.up
+        add_column :functions, :purpose_faith_1, :integer, :default => 0
+	add_column :functions, :purpose_faith_2, :text
+  end
+
+  def self.down
+	remove_column :functions, :purpose_faith_1, :integer, :default => 0
+	remove_column :functions, :purpose_faith_2, :text
+  end
+end
