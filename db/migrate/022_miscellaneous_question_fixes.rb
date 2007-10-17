32197d591770a533308f21971c673392f418c84d
Joe Pym <joe@27stars.co.uk>
2007-10-17 10:08:51 +0000

    Miscellanous view fixes, adding the extra questions from the new question spec

---
+++ db/migrate/022_miscellaneous_question_fixes.rb
@@ -0,0 +1,15 @@
+class MiscellaneousQuestionFixes < ActiveRecord::Migration
+  def self.up
+	add_column :functions, :additional_work_disability_13,  :integer,  :default => 0
+	add_column :functions, :additional_work_disability_14,  :integer,  :default => 0
+	
+	remove_column :functions, :additional_work_gender_11
+  end
+
+  def self.down
+	remove_column :functions, :additional_work_disability_13
+	remove_column :functions, :additional_work_disability_13
+	
+	add_column :functions, :additional_work_gender_11,  :integer,  :default => 0
+  end
+end
