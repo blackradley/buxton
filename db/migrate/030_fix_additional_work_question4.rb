4a743efc6b579f49a89e4cc62d933c1fee6ef97e
Joe Pym <joe@27stars.co.uk>
2007-10-23 09:33:14 +0000

    Summary page now works again

---
+++ db/migrate/030_fix_additional_work_question4.rb
@@ -0,0 +1,19 @@
+class FixAdditionalWorkQuestion4 < ActiveRecord::Migration
+  def self.up
+	change_column :functions, :additional_work_gender_8, :integer, :default => 0
+	change_column :functions, :additional_work_race_8, :integer, :default => 0
+	change_column :functions, :additional_work_disability_8, :integer, :default => 0
+	change_column :functions, :additional_work_age_8, :integer, :default => 0
+	change_column :functions, :additional_work_sexual_orientation_8, :integer, :default => 0
+	change_column :functions, :additional_work_faith_8, :integer, :default => 0
+  end
+
+  def self.down
+	change_column :functions, :additional_work_gender_8, :text
+	change_column :functions, :additional_work_race_8, :integer, :text
+	change_column :functions, :additional_work_disability_8, :text
+	change_column :functions, :additional_work_age_8, :text
+	change_column :functions, :additional_work_sexual_orientation_8, :text
+	change_column :functions, :additional_work_faith_8, :text
+  end
+end
