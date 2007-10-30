295140ebe58c972e4b97501de5d04e84eb5fe651
Karl Doody <karl@27stars.co.uk>
2007-10-30 11:24:46 +0000

    Condensed header.

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,10 +1,7 @@
 #
 # $URL$
-#
 # $Rev$
-#
 # $Author$
-#
 # $Date$
 #
 # Copyright (c) 2007 Black Radley Systems Limited. All rights reserved..
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,17 +1,13 @@
 # 
 # $URL: http://svn3.cvsdude.com/BlackRadley/buxton/trunk/app/models/issue.rb $ 
-# 
 # $Rev: 370 $
-# 
 # $Author: 27Stars-Joe $
-# 
 # $Date: 2007-10-28 12:13:39 +0100 (Tue, 18 Oct 2007) $
+#
+# Copyright (c) 2007 Black Radley Systems Limited. All rights reserved. 
 #
 # An issue is an item that requires an action plan, and is specified in confidence_consultation sections.
 # Each strand has it's own issues, and these issues get taken into account in completed tags and dependent functions.
-#
-# Copyright (c) 2007 Black Radley Systems Limited. All rights reserved. 
-
 class Issue < ActiveRecord::Base
 	belongs_to :function
 end
