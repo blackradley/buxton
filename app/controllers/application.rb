e91ec12142825c3ac595162cd982ceec930fb7b1
Karl Doody <karl@27stars.co.uk>
2007-11-01 15:37:44 +0000

    Removed old comments, re-ordered a require.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,3 @@
-require 'digest/sha1'
 # 
 # $URL$ 
 # $Rev$
@@ -8,15 +7,10 @@ require 'digest/sha1'
 # Copyright (c) 2007 Black Radley Systems Limited. All rights reserved.
 #
 # Filters added to this controller apply to all controllers in the application. 
-# So it is at this point we apply the security (:authenticate) filter.  Well actually we don't
-# because this is just a demo right now.
+# So it is at this point we apply the security (:authenticate) filter.
 # 
-# TODO: Apply the :authenticate filter for real.
-#  
-# In development all the libraries are automatically loaded so the Digest::SHA1 is
-# already available.  However it is not available in production hence the <tt>require</tt>
-# here (well at the top of the page so that RDoc works correctly).
-#  
+require 'digest/sha1'
+
 class ApplicationController < ActionController::Base
   before_filter :authenticate
     
