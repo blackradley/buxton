a6d6f3f25808d8bdf1f2af23a3b184e745ebaba0
Karl Doody <karl@27stars.co.uk>
2007-11-01 16:46:02 +0000

    Put verify in a better place.

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -8,14 +8,14 @@
 #
 class OrganisationsController < ApplicationController
 
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
   def index
     list
     render :action => 'list'
   end
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
 
   # List the organisation for the administrative User.
   def list
