c0ab7a73a48107868006988102dc6183477412ad
Karl Doody <karl@27stars.co.uk>
2007-11-01 14:42:03 +0000

    Some commenting fixes.

--- app/models/function.rb
+++ app/models/function.rb
@@ -16,19 +16,16 @@
 # When a function is created a single User is created for it at the same time,
 # who is listed in the user table.  If the same person (as identified by their
 # email address) is the function manager for a number of functions then they
-# are entered many time into the User table.  You could have a list of users
-# and a list of functions and then attach users to functions.  This seemed 
-# inappropriately complicated since it requires the Organisation manager to
-# manage two separate lists and then join them together.
+# are entered many time into the User table.
 # 
-# The functions are supposed to contributed to organisational goals or
+# The functions are supposed to contribute to organisational goals or
 # strategies hence they link to a FunctionStrategy.  This is not a "has many 
 # and belongs to" where the intervening table would be transparent, because we 
 # want to store a value in the function_strategy table of how much of a contribution
 # is being made to the strategy.
 # 
 # Neither is a "has many: through" used because like the "has many and belongs to"
-# it is not really a many to many relationship.  Each Strategy is owned by 
+# it is not really a many to many relationship. Each Strategy is owned by 
 # the Organisation, and each Function then contributes (or doesn't contribute) to
 # each of the strategies.  The strategy list for each function within an organisation
 # is always the same, and the response is stored in the function_strategy table.
@@ -44,7 +41,6 @@ class Function < ActiveRecord::Base
   validates_associated :organisation
   has_many :function_strategies
   has_many :issues, :dependent => :destroy
-  
   
   def strategy(strategy)
     Strategy.find(strategy)
@@ -168,7 +164,7 @@ class Function < ActiveRecord::Base
     return completed
   end
   
-  #This initilises a statistics object, and scores it.
+  #This initialises a statistics object, and scores it.
   #TODO: Heavy amount of speed increases. No extensive comments as yet, because I'm anticipating ripping this
   #calling method out and replacing it with a much faster version. Statistics library should remain largly unchanged though.
   def statistics
