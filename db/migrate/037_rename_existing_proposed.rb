a9d09d5113f3e71cecdcc1828c5a7de445311ca3
Karl Doody <karl@27stars.co.uk>
2007-11-07 16:59:28 +0000

    Added copyright header to some new files.

--- db/migrate/037_rename_existing_proposed.rb
+++ db/migrate/037_rename_existing_proposed.rb
@@ -1,3 +1,11 @@
+#  
+# $URL$ 
+# $Rev$
+# $Author$
+# $Date$
+#
+# Copyright (c) 2007 Black Radley Systems Limited. All rights reserved.
+#
 class RenameExistingProposed < ActiveRecord::Migration
   def self.up
 	rename_column :functions, :purpose_overall_1, :existing_proposed
