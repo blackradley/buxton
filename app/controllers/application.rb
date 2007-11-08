210c1accc8ef52eeb8191f335645a940c209996d
Karl Doody <karl@27stars.co.uk>
2007-11-08 09:56:07 +0000

    Jiggled protected/private methods around. Logout now system-wide. Verify on demo controller.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -21,22 +21,27 @@ class ApplicationController < ActionController::Base
   session :session_key => '_buxton_session_id'
 
 protected
-  # Override in controller classes that should require authentication
-  def secure?
-    false
-  end
-
-private
   # Check that the user in the session is for real.
   def authenticate
     if secure? && session[:user_id].nil?
       redirect_to :controller => 'users'
     end
   end
-  
+
   # If the user_id session variable exists, grab this user from the database and store
   # in @current_user available to the action of any controller.
   def set_current_user
     @current_user = User.find(session[:user_id]) unless session[:user_id].nil?
   end
+
+  # Override in controller classes that should require authentication
+  def secure?
+    false
+  end
+
+private
+  # Log the user out of the system by killing the session parameter that identifies them as being logged in
+  def logout
+    session[:user_id] = nil    
+  end  
 end
