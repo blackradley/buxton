bf2ef91e6fa701319329442a8ce04cf946df93f0
Joe Pym <joe@27stars.co.uk>
2007-11-16 16:37:49 +0000

    Speed increase on statistics.

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -15,27 +15,49 @@ require 'stat_question.rb'
 #TODO: Make aliases that point to the function methods..so for example, def stats.impact; @function.impact; end.
 class Statistics
   attr_reader :function
-  def initialize(question_wording_lookup, function)  
+  def initialize  
     topic_hash = {}
-    questions = question_wording_lookup
-    questions.each do |strand_name, strand|
-	    strand.each do |section_name, section|
-		    section.each do |question, value|
-			    topic_hash[strand_name] = {} unless topic_hash[strand_name]
-			    topic_hash[strand_name] ["#{section_name.to_s}_#{strand_name.to_s}_#{question.to_s}".to_sym] = [value[1], value[4]]
-		    end
-	    end
+    hashes = @@Hashes
+    questions = hashes['questions']
+    overall_questions = hashes['overall_questions']
+    strands = hashes['strands']
+    strands.each_key do |strand_name|
+      unless strand_name == 'overall' then
+	      questions.each do |section_name, section|
+		      section.each do |question_name, values|
+			      topic_hash[strand_name] = {} unless topic_hash[strand_name]
+			      topic_hash[strand_name] ["#{section_name.to_s}_#{strand_name.to_s}_#{question_name.to_s}".to_sym] = [values['type'], values['weights']]
+		      end
+	      end
+      else
+        overall_questions.each do |section_name, section|
+          section.each do |question_name, values|
+            topic_hash[strand_name] = {} unless topic_hash[strand_name]
+            topic_hash[strand_name] ["#{section_name.to_s}_#{strand_name.to_s}_#{question_name.to_s}".to_sym] = [values['type'], values['weights']]
+          end
+        end        
+      end
     end
     #replace all the symbol references to lookups with the lookup itself, and initialize the question.
-    topic_hash.each{|topic_name, topic| topic.each{|question, value| topic_hash[topic_name][question] = StatQuestion.new(value, question, function)}}
+    topic_hash.each{|topic_name, topic| topic.each{|question, value| topic_hash[topic_name][question] = StatQuestion.new(value, question, hashes)}}
     #replace all the topic symbols with a StatTopic object
     stat_topics = {}
-    questions.each_key{|strand_name| stat_topics[strand_name] = StatTopic.new(topic_hash[strand_name], strand_name)} 
+    strands.each_key{|strand_name| stat_topics[strand_name] = StatTopic.new(topic_hash[strand_name], strand_name)} 
     #create a StatFunction from the hash
-    @function = StatFunction.new(stat_topics, function)
+    @function = StatFunction.new(stat_topics, hashes)
   end
   #TODO: Since it now is passed the function, it should be able to calculate the score itself. This will make the model code much simpler.
-  def score(results)
-    @function.score(results) if results
+  def score(results, function)
+    @function.score(results, function) if results
+  end
+  #3 methods to provide an interface into it for easier readability.
+  def impact
+    @function.impact
+  end
+  def priority_ranking
+    @function.fun_priority_ranking
+  end
+  def relevance
+    @function.fun_relevance
   end
 end
