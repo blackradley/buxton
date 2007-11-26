edb194f347b1a8130f44b9804b109ab751512994
Joe Pym <joe@27stars.co.uk>
2007-11-26 16:15:56 +0000

    Strategies now appear in edit purpose and summary pages.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -7,18 +7,6 @@
 # Copyright (c) 2007 Black Radley Systems Limited. All rights reserved. 
 #
 module SectionsHelper
-  #
-  # If the strategy response is not set for a function, return 0 instead.
-  # 
-  def strategy_response_or_zero(function_responses, id)
-    function_response = function_responses.find_by_strategy_id(id)
-    if function_response.nil?
-      return 0
-    else
-      return function_response.strategy_response
-    end
-  end
-  
   def insert_help(section, strand, question)
     strand = strand.to_sym
     divId="help_#{section}_#{strand}_#{question}"
