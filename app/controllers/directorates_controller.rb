c188634424e9e5aa63532953f8b390fa248007e3
Joe Pym <joe@27stars.co.uk>
2007-11-30 15:24:18 +0000

    Adding the directorates this time.

---
+++ app/controllers/directorates_controller.rb
@@ -0,0 +1,27 @@
+class DirectoratesController < ApplicationController
+def new
+    @directorate = Directorate.new
+end
+
+def create
+    @directorate = Directorate.new(params[:directorate])
+    begin
+      Directorate.transaction do
+        @directorate.save!
+      end
+    rescue ActiveRecord::RecordNotSaved
+        flash[:notice] = "Directorate creation failed. Please try again, and if it continues to fail, contact an administrator."
+        render :action => :new, :controller => 'organisations'    
+    end
+ end
+  
+def destroy
+    @directorate = @current_user.organisation.directorates.find(params[:id])
+    @directorate.destroy
+
+    respond_to do |format|
+      format.js
+    end
+end
+
+end
