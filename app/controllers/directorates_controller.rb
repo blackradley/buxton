6e33226e18334331fe2b9ca4daa5ee368561f734
Heather Thomas <heather@27stars.co.uk>
2007-12-03 11:16:03 +0000

    styling for edit and show organisations

--- app/controllers/directorates_controller.rb
+++ app/controllers/directorates_controller.rb
@@ -3,25 +3,34 @@ def new
     @directorate = Directorate.new
 end
 
-def create
-    @directorate = Directorate.new(params[:directorate])
-    begin
-      Directorate.transaction do
-        @directorate.save!
-      end
-    rescue ActiveRecord::RecordNotSaved
-        flash[:notice] = "Directorate creation failed. Please try again, and if it continues to fail, contact an administrator."
-        render :action => :new, :controller => 'organisations'    
+# def create
+#     @directorate = Directorate.new(params[:directorate])
+#     begin
+#       Directorate.transaction do
+#         @directorate.save!
+#       end
+#     rescue ActiveRecord::RecordNotSaved
+#         flash[:notice] = "Directorate creation failed. Please try again, and if it continues to fail, contact an administrator."
+#         render :action => :new, :controller => 'organisations'    
+#     end
+#  end
+ 
+ def create
+   
+    @directorate = Organisation.find(params[:org_id]).directorates.build(params[:directorate])
+    @directorate.save
+
+    respond_to do |format|
+      format.js
     end
- end
+  end
   
 def destroy
-    @directorate = @current_user.organisation.directorates.find(params[:id])
+    @directorate = Directorate.find(params[:id])
     @directorate.destroy
 
     respond_to do |format|
       format.js
     end
 end
-
 end
