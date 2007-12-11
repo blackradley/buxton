283a52481aa6395466f6a82b0567d725732ef278
Karl Doody <karl@27stars.co.uk>
2007-12-11 11:13:33 +0000

    Addded and modified core Rails files for Rails 2.0.1 benefits.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors - bad email addresses should just be ignored
+# Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = false
 
 # Use SMTP protocol to deliver emails
