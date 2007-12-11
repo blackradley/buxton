83f5c91198f1c67e3c3680ec9fe4eaba944f6aae
Karl Doody <karl@27stars.co.uk>
2007-12-11 11:13:33 +0000

    Addded and modified core Rails files for Rails 2.0.1 benefits.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,15 +21,12 @@ config.whiny_nils = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.action_view.cache_template_extensions         = false
-config.action_view.debug_rjs                         = true
-
-# Care if the mailer can't send
-config.action_mailer.raise_delivery_errors = true
-
-# Use SMTP protocol to deliver emails
-config.action_mailer.delivery_method = :smtp
 
 # Don't send emails
 config.action_mailer.perform_deliveries = false
+
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
