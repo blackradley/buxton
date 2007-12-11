7cb80eef95509c638512f2dfe5703edf3994ba8e
Joe Pym <joe@27stars.co.uk>
2007-12-11 17:00:17 +0000

    reworded the library

--- config/initializers/migration_style_library_edits.rb
+++ config/initializers/migration_style_library_edits.rb
@@ -2,10 +2,10 @@ module Kernel
   alias_method :standard_method_missing, :method_missing
   def method_missing(meth, *args)
     begin
-      File.open("#{RAILS_ROOT}/lib/#{meth.to_s}_up.rb", 'w'){}
+      File.open("#{RAILS_ROOT}/lib/#{meth.to_s}_up.rb", 'r'){}
+      File.open("#{RAILS_ROOT}/lib/#{meth.to_s}_down.rb", 'r'){}
       load "#{RAILS_ROOT}/lib/#{meth.to_s}_up.rb"
       yield
-      File.open("#{RAILS_ROOT}/lib/#{meth.to_s}_down.rb", 'w'){}
       load "#{RAILS_ROOT}/lib/#{meth.to_s}_down.rb"
     rescue
       standard_method_missing(meth, *args)
