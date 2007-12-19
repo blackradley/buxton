d8d99ac18743ffe06364cdbde74aba7e66aa064e
Karl Doody <karl@27stars.co.uk>
2007-12-19 11:01:47 +0000

    formatting fix for summary toggles

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,9 +15,14 @@ config.cache_classes = true
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_extensions         = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
