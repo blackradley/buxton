737b09fb9083d75bc3f7790230157d2673636787
Joe Pym <joe@27stars.co.uk>
2008-01-04 16:12:11 +0000

    calc rewrite, white spacing, final removing of impact overall with migration

---
+++ db/migrate/059_removing_overall_impact.rb
@@ -0,0 +1,13 @@
+class RemovingOverallImpact < ActiveRecord::Migration
+  def self.up
+    remove_column :activities, :impact_overall_1
+    remove_column :activities, :impact_overall_2
+    remove_column :activities, :impact_overall_3
+  end
+
+  def self.down
+    add_column :activities, :impact_overall_1, :integer, :default => 0
+    add_column :activities, :impact_overall_2, :integer, :default => 0
+    add_column :activities, :impact_overall_3, :text
+  end
+end
