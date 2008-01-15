42d85dec89584a97f2da8e875b184d3e171630b8
Karl Doody <karl@27stars.co.uk>
2008-01-15 12:15:47 +0000

    Added Question model, migration and associations.

---
+++ db/migrate/065_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration
+  def self.up
+    create_table :questions, :id => false do |t|
+      t.integer :activity_id
+      t.string :name
+      t.boolean :completed, :default => false
+    end
+  end
+
+  def self.down
+    drop_table :questions
+  end
+end
