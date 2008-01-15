96655919df9c548be5f22b2de01d50ce7644098e
Joe Pym <joe@27stars.co.uk>
2008-01-15 15:34:19 +0000

    After update working as of before. Now significantly faster activity saves.

--- config/initializers/questions_and_statistics.rb
+++ config/initializers/questions_and_statistics.rb
@@ -2,3 +2,15 @@
 @@Hashes = YAML.load_file("#{RAILS_ROOT}/config/questions.yml")
 @@question_max = 0
 Activity.force_question_max_calculation
+@@dependencies = {}
+@@Hashes['questions'].each do |section, section_data|
+  section_data.each do |question_name, question_data|
+    @@Hashes['wordings'].keys.each do |strand|
+      unless question_data.values[4].blank? then
+        temp = question_data.values[4].split(" ")
+        temp[0]=  eval(%Q{<<"DELIM"\n} + temp[0] + "\nDELIM").chomp
+        @@dependencies[temp[0]] = ["#{section}_#{strand}_#{question_name}", temp[1]]
+      end
+    end
+  end
+end
