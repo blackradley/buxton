adfa413507f1670cc8b1a9dbf8f3b9f10dce7acf
Karl Doody <karl@27stars.co.uk>
2008-01-29 16:25:08 +0000

    Beginnings of a Capistrano multi-stage setup.

---
+++ config/deploy/staging.rb
@@ -0,0 +1,14 @@
+# =============================================================================
+# STAGING SPECIFIC VARIABLES
+# =============================================================================
+set :domain, "impactstaging.org.uk"
+
+# =============================================================================
+# THIS WOULD BE IN DEPLOY.RB IF IT COULD BE LAZY EVALUATED, BUT IT CAN'T
+# SO IT MUST GO HERE :/ MAKE SURE STAGING.RB AND PRODUCTION.RB ARE THE SAME
+# =============================================================================
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+set :deploy_to, "/var/www/vhosts/#{domain}/rails/testapp"
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml" # required really but needs to be after deploy_to
