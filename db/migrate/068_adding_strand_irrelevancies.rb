1e33bd816511306d50f3dc1525a07c1f0cc0c827
Joe Pym <joe@27stars.co.uk>
2008-02-04 16:11:44 +0000

    Fixed dependencies, added limited reduced strands

---
+++ db/migrate/068_adding_strand_irrelevancies.rb
@@ -0,0 +1,19 @@
+class AddingStrandIrrelevancies < ActiveRecord::Migration
+  def self.up
+    add_column :activities, :gender_irrelevant, :boolean, :default => false
+    add_column :activities, :sexual_orientation_irrelevant, :boolean, :default => false
+    add_column :activities, :age_irrelevant, :boolean, :default => false
+    add_column :activities, :faith_irrelevant, :boolean, :default => false
+    add_column :activities, :disability_irrelevant, :boolean, :default => false
+    add_column :activities, :race_irrelevant, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :activities, :gender_irrelevant
+    remove_column :activities, :sexual_orientation_irrelevant
+    remove_column :activities, :age_irrelevant
+    remove_column :activities, :faith_irrelevant
+    remove_column :activities, :disability_irrelevant
+    remove_column :activities, :race_irrelevant
+  end
+end
