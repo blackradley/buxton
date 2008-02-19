17303f31ee251693f1b77b9899d46d68d14738da
Joe Pym <joe@27stars.co.uk>
2008-02-19 16:49:33 +0000

    Fixes to 123 page

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -135,7 +135,7 @@ class SectionsController < ApplicationController
     end
     
     flash[:notice] =  "#{@activity.name} was successfully updated."
-    redirect_to :back
+    redirect_to :controller => 'activities', :action => 'activity_type'
     
   rescue ActiveRecord::RecordNotSaved, ActiveRecord::RecordInvalid
     flash[:notice] =  "Could not update the activity."
