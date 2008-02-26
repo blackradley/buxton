70f7dbb1f25b2254bb5a98596f68f1d55d86cf5b
Joe Pym <joe@27stars.co.uk>
2008-02-26 10:56:22 +0000

    Altered completed tags and need status calculations, corrected purpose loop in initializers

--- config/initializers/questions_and_statistics.rb
+++ config/initializers/questions_and_statistics.rb
@@ -24,13 +24,11 @@ Activity.force_question_max_calculation
 end
 @@Hashes['overall_questions'].each do |section, section_data|
   section_data.each do |question_name, question_data|
-    @@Hashes['wordings'].keys.each do |strand|
-      unless question_data['dependent_questions'].blank? then
-        temp = question_data['dependent_questions'].split(" ")
-        temp[0]=  eval(%Q{<<"DELIM"\n} + temp[0] + "\nDELIM").chomp
-        @@dependencies[temp[0]] = [] if @@dependencies[temp[0]].nil?
-        @@dependencies[temp[0]].push(["#{section}_overall_#{question_name}", temp[1]])
-      end
+    unless question_data['dependent_questions'].blank? then
+      temp = question_data['dependent_questions'].split(" ")
+      temp[0]=  eval(%Q{<<"DELIM"\n} + temp[0] + "\nDELIM").chomp
+      @@dependencies[temp[0]] = [] if @@dependencies[temp[0]].nil?
+      @@dependencies[temp[0]].push(["#{section}_overall_#{question_name}", temp[1]])
     end
   end
 end
