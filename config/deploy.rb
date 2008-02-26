26eb0c564565ff34cad79a0c292409acb57cb018
Karl Doody <karl@27stars.co.uk>
2008-02-26 10:36:43 +0000

    Updated one-click deployment to include server restarting and running of migrations. Staging server SSHD setup modified to work with Capistrano's non-interactive shell which, by default, doesn't load the user environment. PermitUserEnvironment changed to yes in sshd_config and the environment variables we need set up in ~/.ssh/environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,11 +31,44 @@ namespace :db do
 end
 
 # =============================================================================
-# TASKS UNTIL CAPISTRANO 2 SUPPORTS MONGREL OUT-OF-THE-BOX
+# TASKS FOR MONGREL UNTIL CAP 2 SUPPORTS IT NATIVELY
 # =============================================================================
 namespace :deploy do
-  desc "Restart the mongrel cluster"
-  task :restart, :roles => :app do
-    invoke_command "/etc/init.d/mongrel_cluster restart"
+  namespace :mongrel do
+    [ :stop, :start, :restart ].each do |t|
+      desc "#{t.to_s.capitalize} the mongrel appserver"
+      task t, :roles => :app do
+        #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
+        invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
+      end
+    end
+  end
+
+  desc "Custom restart task for mongrel cluster"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.restart
+  end
+
+  desc "Custom start task for mongrel cluster"
+  task :start, :roles => :app do
+    deploy.mongrel.start
+  end
+
+  desc "Custom stop task for mongrel cluster"
+  task :stop, :roles => :app do
+    deploy.mongrel.stop
+  end
+
+end
+
+# =============================================================================
+# ENVIRONMENT DEBUGGING
+# =============================================================================
+desc "Echo environment vars" 
+namespace :env do
+  task :echo do
+    run "echo printing out cap info on remote server"
+    run "echo $PATH"
+    run "printenv"
   end
 end
