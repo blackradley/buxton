db20e3994d20aea8d7a1bc7db9763f6d9d0c3093
Joe Pym <joe@27stars.co.uk>
2008-03-17 12:15:15 +0000

    Fixed comments and notes if you submit blank string

--- public/javascripts/commentsandnotes.js
+++ public/javascripts/commentsandnotes.js
@@ -1,7 +1,14 @@
 function edit_comment(question_id){
-  Element.toggle(question_id + '_comment_view');
-  Element.toggle(question_id + '_comment_form');
-  $(question_id + '_comment_fill_in').focus();
+  if(trim_using_recursion(document.getElementById(question_id + '_comment').innerHTML).length > 0){
+    Element.toggle(question_id + '_comment_view');
+    Element.toggle(question_id + '_comment_form');
+    $(question_id + '_comment_fill_in').focus();
+  }
+  else{
+   document.getElementById(question_id + '_comment_new').style.display = 'block';
+   document.getElementById(question_id + '_comment_form').style.display = 'none';
+   document.getElementById(question_id + '_comment_view').style.display = 'none';    
+  }
 }
 function new_comment(question_id){
   Element.toggle(question_id + '_comment_new');
@@ -29,9 +36,16 @@ function trim_using_recursion(str)
 }
 
 function edit_note(question_id){
-  Element.toggle(question_id + '_note_view');
-  Element.toggle(question_id + '_note_form');
-  $(question_id + '_note_fill_in').focus();
+  if(trim_using_recursion(document.getElementById(question_id + '_note').innerHTML).length > 0){
+    Element.toggle(question_id + '_note_view');
+    Element.toggle(question_id + '_note_form');
+    $(question_id + '_note_fill_in').focus();
+  }
+  else{
+    document.getElementById(question_id + '_note_new').style.display = 'block';
+    document.getElementById(question_id + '_note_form').style.display = 'none';
+    document.getElementById(question_id + '_note_view').style.display = 'none';
+  }
 }
 function new_note(question_id){
   Element.toggle(question_id + '_note_new');
