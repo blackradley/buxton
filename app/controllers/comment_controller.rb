939142e026b3085f5d5ade141343c420fb166673
Joe Pym <joe@27stars.co.uk>
2008-03-17 12:15:15 +0000

    Fixed comments and notes if you submit blank string

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,15 +1,15 @@
 class CommentController < ApplicationController
   
   def edit
-    return if params[:comment].blank?
-    puts params
     parent_question = @current_user.activity.questions.find(params[:question_id])
     unless parent_question.comment then
       parent_question.build_comment(:contents => params[:comment]).save!
     else
       parent_question.comment.update_attributes(:contents => params[:comment])
     end
-    render :inline => @current_user.activity.questions.find(params[:question_id]).comment.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, "")
+    text_to_render = @current_user.activity.questions.find(params[:question_id]).comment.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, "")
+    text_to_render = " " if params[:comment].blank?
+    render :inline => text_to_render
   end
   
   def destroy
--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -1,12 +1,14 @@
 class NoteController < ApplicationController
   def edit
-    parent_question = Question.find(params[:question_id])
+    parent_question = @current_user.activity.questions.find(params[:question_id])
     unless parent_question.note then
-      Note.new(:contents => params[:note], :question_id => parent_question.id).save!
+      parent_question.build_note(:contents => params[:note]).save!
     else
       parent_question.note.update_attributes(:contents => params[:note])
     end
-    render :inline => Question.find(params[:question_id]).note.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, "")
+    text_to_render = @current_user.activity.questions.find(params[:question_id]).note.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, "")
+    text_to_render = " " if params[:note].blank?
+    render :inline => text_to_render
   end
   def destroy
     parent_question = Question.find(params[:question_id])
