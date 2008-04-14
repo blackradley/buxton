b1ccb770bb882561821162a3930cbfbfd8cc3703
Karl Doody <karl@27stars.co.uk>
2008-04-14 12:29:40 +0000

    Refactored showing question answers. Partially HAMLized. Less duplication. Added ' around some of the text in questions.yml

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,10 @@ class Question < ActiveRecord::Base
   has_one :comment, :dependent => :destroy
   has_one :note, :dependent => :destroy
   validates_uniqueness_of :name, :scope => :activity_id
+  attr_reader :section, :strand, :number
+  
+  def after_initialize
+    @section, @strand, @number = Activity.question_separation(self.name)
+  end
+  
 end
