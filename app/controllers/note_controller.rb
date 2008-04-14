e9bbda03dc770e3574cbcb6681d00c62b1f37dd6
Joe Pym <joe@27stars.co.uk>
2008-04-14 13:59:04 +0000

    Added comments and notes to strategies.

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -15,4 +15,20 @@ class NoteController < ApplicationController
     parent_question.note.destroy if parent_question && parent_question.note
     render :inline => ''
   end
+  def edit_strategy
+    parent_strategy = @current_user.activity.activity_strategies.find(params[:activity_strategy_id])
+    unless parent_strategy.note then
+      parent_strategy.build_note(:contents => params[:note]).save!
+    else
+      parent_strategy.note.update_attributes(:contents => params[:note])
+    end
+    text_to_render = @current_user.activity.activity_strategies.find(params[:activity_strategy_id]).note.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, "")
+    text_to_render = " " if params[:note].blank?
+    render :inline => text_to_render
+  end
+  def destroy_strategy
+    parent_strategy = ActivityStrategy.find(params[:activity_strategy_id])
+    parent_strategy.note.destroy if parent_strategy && parent_strategy.note
+    render :inline => ''
+  end
 end
