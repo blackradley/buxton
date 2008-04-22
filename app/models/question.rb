318fbdc3494d48377db9ab476759feba16728483
Joe Pym <joe@27stars.co.uk>
2008-04-22 11:02:55 +0000

    Fixed some snags, fixed comments on pdf megapages, mocked up the database for questions.

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,9 +4,49 @@ class Question < ActiveRecord::Base
   has_one :note, :dependent => :destroy
   validates_uniqueness_of :name, :scope => :activity_id
   attr_reader :section, :strand, :number
-  
-  def after_initialize
-    @section, @strand, @number = Activity.question_separation(self.name)
+  def weights
+    [*@@Hashes['weights'][self.activity.question_wording_lookup(section, strand, number)[4]]].map(&:to_i)
   end
-  
+
+  def weight
+    weights[self.response.to_i]
+  end
+
+  def invisible?
+    (!@@invisible_questions.include?(self.name) && self.activity.existing_proposed == 2)
+  end
+
+  def response
+    self.activity.send(self.name)
+  end
+
+  def section
+    return @section if @section
+    @section, @strand, @number = Question.fast_split(self.name)
+    @section
+  end
+
+  def strand
+    return @strand if @strand
+    @section, @strand, @number = Question.fast_split(self.name)
+    @strand
+  end
+
+  def number
+    return @number if @number
+    @section, @strand, @number = Question.fast_split(self.name)
+    @number
+  end
+
+  #44 seconds for 1M iterations
+  private
+  def self.fast_split(string)
+    splits = string.split("_")
+    number_to_return = splits.last
+    section_to_return = @@Hashes['questions'].keys.select{|key| key.include?(splits.first)}.first
+    return [nil, nil, nil] if section_to_return == nil
+    strand_to_return = (splits - section_to_return.split("_") - [number_to_return]).join("_")
+    return [section_to_return, strand_to_return, number_to_return]
+  end
+
 end
