0eaf51c372d57aee1b987d639c7d715d8a76662e
Karl Doody <karl@27stars.co.uk>
2008-05-07 09:45:24 +0000

    Latest video scripts

--- videos/am_impact.rb
+++ videos/am_impact.rb
@@ -9,14 +9,19 @@ launch "Mousepose"
 if LIVE
   plugin "ishowu"
   ishowu_set_region at(84, 34, 1056, 800)
-  ishowu_start_recording
 end
 
 launch 'Safari', at(100, 50, 1024, 768)
-url 'http://localhost:3000/cc533d9de98744a379c47b8df037242c6738ed4f'
+# url 'http://localhost:3000/cc533d9de98744a379c47b8df037242c6738ed4f'
+url 'http://192.168.0.2:3000/cc533d9de98744a379c47b8df037242c6738ed4f'
 pause 4
-url 'http://localhost:3000/activities/questions'
+url 'http://192.168.0.2:3000/activities/questions'
 pause 8
+
+if LIVE
+  ishowu_start_recording
+  pause 4
+end
 
 # Click on disability edit pencil.
 move to_element('#disability_impact_completed')
@@ -78,7 +83,7 @@ drag by(0, 30)
 move to_element('#section_save_button')
 click
 
-pause 10
+pause 5
 
 # Show the green tick on the questions page.
 move to_element('#disability_impact_completed')
--- videos/am_login_alt.rb
+++ videos/am_login_alt.rb
@@ -14,25 +14,26 @@ end
 
 # Act Mgr log in for ‘Develop service user engagement strategy’
 launch 'Safari', at(100, 50, 1024, 768)
+pause 3
 url 'http://192.168.0.2:3000/cc533d9de98744a379c47b8df037242c6738ed4f'
 
 pause 4
 
 # Click no for CES question and then click on link to CES.
 move to_element('#activity_ces_question')
-pause 1
+pause 3
 
 highlight do
-  pause 2
+  pause 3
   # Answer CES question 'No'
-  drag by(0, 20)
+  drag by(0, 30)
   
   # Pause a bit
-  pause 2
+  pause 3
 
   # Click on the submit button
   move to_element('#next_button')
-  pause 1
+  pause 3
   click
 end
 
@@ -40,25 +41,25 @@ pause 3
 
 # Click next, then Policy, then Proposed, then save, show section 2 appearing.
 move to_element('#activity_function_policy')
-pause 1
+pause 3
 
 highlight do
-  pause 2  
+  pause 3  
   # Select proposed policy
-  drag by(0, 20)
+  drag by(0, 30)
   
   # Pause a bit
-  pause 1
+  pause 3
     
   move to_element('#activity_existing_proposed')
-  pause 1
-  drag by(0, 20)
+  pause 3
+  drag by(0, 30)
   
   # Pause a bit
-  pause 2  
+  pause 3  
 
   move to_element('#save_activity_type_button')
-  pause 1
+  pause 3
   click
 end
 
