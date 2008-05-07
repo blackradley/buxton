83f149ee567e59d32dbbeec6f6bf9aca22dd3a23
Karl Doody <karl@27stars.co.uk>
2008-05-07 09:45:24 +0000

    Latest video scripts

--- videos/am_login.rb
+++ videos/am_login.rb
@@ -16,6 +16,7 @@ end
 
 # Clicks on the access key for a new activity
 launch 'Safari', at(100, 50, 1024, 768)
+pause 3
 url 'http://192.168.0.2:3000/cc533d9de98744a379c47b8df037242c6738ed4f'
 
 pause 4
@@ -25,12 +26,12 @@ move to_element('#activity_ces_question')
 pause 1
 
 highlight do
-  pause 2
+  pause 3
   # Answer CES question 'No'
-  drag by(0, 20)
+  drag by(0, 30)
   
   # Pause a bit
-  pause 2
+  pause 3
 
   # Click on the submit button
   move to_element('#next_button')
@@ -46,19 +47,19 @@ move to_element('#activity_function_policy')
 pause 1
 
 highlight do
-  pause 2  
+  pause 3  
   # Select existing function
-  drag by(0, 10)
+  drag by(0, 15)
   
   # Pause a bit
   pause 1
     
   move to_element('#activity_existing_proposed')
   pause 1
-  drag by(0, 10)
+  drag by(0, 15)
   
   # Pause a bit
-  pause 2  
+  pause 3  
 
   move to_element('#save_activity_type_button')
   pause 1
--- videos/om_overview.rb
+++ videos/om_overview.rb
@@ -17,12 +17,33 @@ end
 launch 'Safari', at(100, 50, 1024, 768)
 url 'http://192.168.0.2:3000/513e159835701a44c12f0035571d1cb90f5da693'
 
-pause 4
+pause 6
+
+if LIVE
+  ishowu_start_recording
+  pause 4
+end
 
 # Click to activities page to show the list of the four activities.
-
+move to_element('#menuBar ul li', :index => 1)
+pause 3
+click
+move by(0, 30)
+pause 3
 
 # Show how Org Mgr can send a key.
-
+move to_element('.actions a', :index => 1)
+pause 3
+click
+pause 3
+move to(815, 315)
+pause 3
+click
+pause 4
 
 # Click on sections page to show how Org Mgr can view questions that have been answered.
+move to_element('#menuBar ul li', :index => 2)
+pause 3
+click
+move by(0, 30)
+pause 3
