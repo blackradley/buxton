3e2113eb1efefa822dae7685f670c469610f5112
Joe Pym <joe@27stars.co.uk>
2008-05-15 09:45:21 +0000

    Fixed completed statuses

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,7 +18,7 @@ class Question < ActiveRecord::Base
   end
 
   def response
-    self.activity.send(self.name)
+    self.activity.send(self.name.to_sym)
   end
 
   def section
@@ -37,6 +37,50 @@ class Question < ActiveRecord::Base
     return @number if @number
     @section, @strand, @number = Question.fast_split(self.name)
     @number
+  end
+  
+  def parent
+    self.activity.questions.find_by_name(Question.parents(self.name.to_s)[0].to_s)
+  end
+  
+  def parent_value
+    Question.parents(self.name.to_s)[1]
+  end
+  
+  def children
+    Question.dependencies(self.name.to_s).map{|child| self.activity.questions.find_by_name(child[0].to_s)}
+  end
+
+  def check_response #Check response verifies whether a response to a question is correct or not.
+    checker = !(response.to_i == 0)
+    checker = ((response.to_s.length > 0)&&response.to_s != "0") unless checker
+    return checker
+  end
+  
+  def check_needed
+    parent_okay = self.parent.nil? ? true : (self.parent.response.to_s == parent_value.to_s)
+    parent_okay && invisible?
+  end
+  
+  def update_status
+    self.completed = check_response
+    self.needed = check_needed
+    save!
+    children.each do |child|
+      child.update_status
+    end
+  end
+  
+  #global methods pertaining to questions
+  def self.dependencies(question = nil)
+    return @@dependencies.clone unless question
+    return @@dependencies[question].clone if @@dependencies[question]
+    []
+  end
+  def self.parents(question = nil)
+    return @@parents.clone unless question
+    return @@parents[question].clone if @@parents[question]
+    []
   end
 
   #44 seconds for 1M iterations
