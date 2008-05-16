b5ebbfc48b4baac921934be3da6691ca1627842d
Joe Pym <joe@27stars.co.uk>
2008-05-16 10:25:38 +0000

    Removed unnecessary activity columns

---
+++ db/migrate/101_removing_unneeded_activity_columns.rb
@@ -0,0 +1,17 @@
+class RemovingUnneededActivityColumns < ActiveRecord::Migration
+  def self.up
+    remove_column :activities, :overall_completed_strategies
+    remove_column :activities, :overall_completed_questions
+    remove_column :activities, :impact
+    remove_column :activities, :overall_started
+    remove_column :activities, :overall_completed_issues
+  end
+
+  def self.down
+    add_column :activities, :overall_completed_strategies, :boolean, :default => false
+    add_column :activities, :overall_completed_questions, :boolean, :default => false
+    add_column :activities, :overall_completed_issues, :boolean, :default => false
+    add_column :activities, :impact, :integer
+    add_column :activities, :overall_started, :boolean, :default => false
+  end
+end
