8da91955cce45d8c7995fc07ce373202b232fd04
Karl Doody <karl@27stars.co.uk>
2008-05-21 09:42:48 +0000

    Piston imported FixtureReplacement plugin. Set up example data base file.

---
+++ db/example_data.rb
@@ -0,0 +1,62 @@
+module FixtureReplacement
+  attributes_for :activity do |a|
+    
+	end
+
+  attributes_for :activity_strategy do |a|
+    
+	end
+
+  attributes_for :comment do |a|
+    
+	end
+
+  attributes_for :directorate do |a|
+    
+	end
+
+  attributes_for :help_text do |a|
+    
+	end
+
+  attributes_for :issue do |a|
+    
+	end
+
+  attributes_for :log do |a|
+    
+	end
+
+  attributes_for :note do |a|
+    
+	end
+
+  attributes_for :organisation_terminology do |a|
+    
+	end
+
+  attributes_for :organisation do |a|
+    
+	end
+
+  attributes_for :project do |a|
+    
+	end
+
+  attributes_for :question do |a|
+    
+	end
+
+  attributes_for :strategy do |a|
+    
+	end
+
+  attributes_for :terminology do |a|
+    
+	end
+
+  attributes_for :user do |a|
+    
+	end
+
+end
