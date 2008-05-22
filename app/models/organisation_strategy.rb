8642596fbb896a442343b9671838e3440f489c36
Joe Pym <joe@27stars.co.uk>
2008-05-22 10:23:02 +0000

    On creation of a project strategy, an activity strategy is created for it and set to 0

--- app/models/organisation_strategy.rb
+++ app/models/organisation_strategy.rb
@@ -7,4 +7,9 @@ class OrganisationStrategy < Strategy
     should_destroy.to_i == 1
   end
 
+  def after_create
+    self.organisation.activities.each do |activity|
+      activity.activity_strategies.build(:strategy_id => self.id, :strategy_response => 0).save
+    end
+  end
 end
