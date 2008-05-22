c62f4bf42dfe732532ac43e2bc00e24b9070722d
Karl Doody <karl@27stars.co.uk>
2008-05-22 18:27:33 +0000

    Updated plugins to their new versions.

--- vendor/plugins/will_paginate/lib/will_paginate.rb
+++ vendor/plugins/will_paginate/lib/will_paginate.rb
@@ -9,31 +9,40 @@ require 'active_support'
 # Happy paginating!
 module WillPaginate
   class << self
+    # shortcut for <tt>enable_actionpack; enable_activerecord</tt>
     def enable
       enable_actionpack
       enable_activerecord
     end
     
+    # mixes in WillPaginate::ViewHelpers in ActionView::Base
     def enable_actionpack
       return if ActionView::Base.instance_methods.include? 'will_paginate'
       require 'will_paginate/view_helpers'
       ActionView::Base.class_eval { include ViewHelpers }
+
+      if ActionController::Base.respond_to? :rescue_responses
+        ActionController::Base.rescue_responses['WillPaginate::InvalidPage'] = :not_found
+      end
     end
     
+    # mixes in WillPaginate::Finder in ActiveRecord::Base and classes that deal
+    # with associations
     def enable_activerecord
       return if ActiveRecord::Base.respond_to? :paginate
       require 'will_paginate/finder'
       ActiveRecord::Base.class_eval { include Finder }
 
+      # support paginating finders on associations
       associations = ActiveRecord::Associations
       collection = associations::AssociationCollection
+      classes = [collection]
+      # before [9200], HMT wasn't a subclass of AssociationCollection
+      unless associations::HasManyThroughAssociation.superclass == collection
+        classes << associations::HasManyThroughAssociation
+      end
       
-      # to support paginating finders on associations, we have to mix in the
-      # method_missing magic from WillPaginate::Finder::ClassMethods to AssociationProxy
-      # subclasses, but in a different way for Rails 1.2.x and 2.0
-      (collection.instance_methods.include?(:create!) ?
-        collection : collection.subclasses.map(&:constantize)
-      ).push(associations::HasManyThroughAssociation).each do |klass|
+      classes.each do |klass|
         klass.class_eval do
           include Finder::ClassMethods
           alias_method_chain :method_missing, :paginate
@@ -42,7 +51,7 @@ module WillPaginate
     end
   end
 
-  module Deprecation
+  module Deprecation #:nodoc:
     extend ActiveSupport::Deprecation
 
     def self.warn(message, callstack = caller)
