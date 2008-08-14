e5f69381a7fcdb5235fe16600d4efbf71941e8af
Joe Pym <joe@27stars.co.uk>
2008-08-14 14:09:38 +0000

    Fixed strategy objectives

--- app/models/terminology.rb
+++ app/models/terminology.rb
@@ -7,5 +7,5 @@
 # Copyright (c) 2008 Black Radley Systems Limited. All rights reserved.
 #
 class Terminology < ActiveRecord::Base
-  has_many :organisation_terminologies, :dependent => :destroy
+  has_many :organisation_terminologies, :dependent => :destroy  
 end
