367dd64254685f2c6a4ea45565906d31b42f542c
Karl Doody <karl@27stars.co.uk>
2008-08-15 11:26:06 +0000

    Fixed case sensitivity of includes.

--- db/migrate/103_generating_banners.rb
+++ db/migrate/103_generating_banners.rb
@@ -1,19 +1,17 @@
-require 'rmagick'
-include Magick
 include BannerGeneration
 
 class GeneratingBanners < ActiveRecord::Migration
-  def self.up
-            
+
+  def self.up      
     organisations= Organisation.find(:all)
     organisations.each do |org|
       create_organisation_banner(org.name, org.id)
       puts "#{org.name} banner generated!"
     end
     create_organisation_banner('Demo', 'demo')
-    
   end
 
   def self.down
   end
+  
 end
