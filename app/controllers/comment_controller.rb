cd1159d2a2e409f8314a3c0cb7f5ecc40dc7ff01
Karl Doody <karl@27stars.co.uk>
2008-08-21 11:53:47 +0000

    Made comments/notes more robust against special characters. Refactored to use a shared partial.

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -7,6 +7,7 @@
 # Copyright (c) 2008 Black Radley Systems Limited. All rights reserved.
 #
 class CommentController < ApplicationController
+  helper :sections
 
   def edit
     parent_question = @current_user.activity.questions.find(params[:question_id])
@@ -15,13 +16,12 @@ class CommentController < ApplicationController
     else
       parent_question.comment.update_attributes(:contents => params[:comment])
     end
-    text_to_render = " "
     if params[:comment].strip.blank? then
       parent_question.comment.destroy
+      render :inline => ''
     else
-      text_to_render = CGI.escapeHTML(@current_user.activity.questions.find(params[:question_id]).comment.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, ""))
+      render :partial => 'sections/attachment', :locals => {:text => parent_question.comment.contents}
     end
-    render :inline => text_to_render
   end
 
   def destroy
@@ -37,13 +37,18 @@ class CommentController < ApplicationController
     else
       parent_strategy.comment.update_attributes(:contents => params[:comment])
     end
-    text_to_render = CGI.escapeHTML(@current_user.activity.activity_strategies.find(params[:activity_strategy_id]).comment.contents.gsub(/\S{35}/, '\0<br />').gsub(/<(\script>|script>)/, ""))
-    text_to_render = " " if params[:comment].blank?
-    render :inline => text_to_render
+    if params[:comment].strip.blank? then
+      parent_strategy.comment.destroy
+      render :inline => ''
+    else
+      render :partial => 'sections/attachment', :locals => {:text => parent_strategy.comment.contents}
+    end
   end
+  
   def destroy_strategy
     parent_strategy = ActivityStrategy.find(params[:activity_strategy_id])
     parent_strategy.comment.destroy if parent_strategy && parent_strategy.comment
     render :inline => ''
   end
+  
 end
--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -6,7 +6,9 @@
 #
 # Copyright (c) 2008 Black Radley Systems Limited. All rights reserved. 
 #
+
 module SectionsHelper
+
   def insert_help(section, strand, question)
     strand = strand.to_sym
     divId="help_#{section}_#{strand}_#{question}"
@@ -15,6 +17,15 @@ module SectionsHelper
   end
    
   def split_long_strings(text)
-    text.to_s.gsub(/\S{35}/, '\0<br />')
+    text.to_s.dup.gsub(/\S{35}/, "\0\n")
   end
+  
+  def format_comment(text)
+    text = text.to_s.dup
+    text = h(split_long_strings(text))
+    text.gsub!(/\r\n?/, "\n")  # \r\n and \r -> \n
+    text.gsub!(/\n/, '<br />') # 1 newline   -> br
+    text
+  end
+
 end
