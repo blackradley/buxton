d707c817a28247f59bd1f7f8ded48e1b28c62148
Karl Doody <karl@27stars.co.uk>
2008-08-21 12:08:21 +0000

    Handle special characters in PDFs in tables generation.

--- lib/pdfwriter_extensions.rb
+++ lib/pdfwriter_extensions.rb
@@ -1,11 +1,21 @@
-CONVERTER = Iconv.new( 'ISO-8859-15//IGNORE//TRANSLIT', 'utf-8')
+CONVERTER = Iconv.new('ISO-8859-15//IGNORE//TRANSLIT', 'utf-8')
 
-module PDF  
-  class Writer  
-    alias_method :old_text, :text  
+module PDF
+  class Writer
+    alias_method :old_text, :text
+    alias_method :old_add_text, :add_text
+    alias_method :old_add_text_wrap, :add_text_wrap
 
-    def text(textto, options = {})  
-      old_text(CONVERTER.iconv(textto), options)
+    def text(text, options = {})
+      old_text(CONVERTER.iconv(text), options)
+    end
+    
+    def add_text(x, y, text, size = nil, angle = 0, word_space_adjust = 0)
+      old_add_text(x, y, CONVERTER.iconv(text), size, angle, word_space_adjust)
+    end
+    
+    def add_text_wrap(x, y, width, text, size = nil, justification = :left, angle = 0, test = false)
+      old_add_text_wrap(x, y, width, CONVERTER.iconv(text), size, justification, angle, test)
     end
   end  
 end
