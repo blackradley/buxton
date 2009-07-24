dc220987c287194e1ef1e043ce00b38412fb4ea6
Joe Pym <joe@27stars.co.uk>
2009-07-24 14:50:05 +0100

    Started regexp fun

--- app/models/login_log.rb
+++ app/models/login_log.rb
@@ -8,4 +8,22 @@
 #
 class LoginLog < Log  
   ICON = 'icons/login.gif'
+  
+  def details
+    det = {}
+    unless message.include?('activity creation screen')
+      data = /<a href='mailto:(\S*)'>email@email.com<\/a>, activity manager of <strong>(\S*)<\/strong> for <strong>(\S*)<\/strong> logged in./.match(self.message)
+      det[:user] = data[1]
+      det[:level] = User.find_by_email(data[1]).class.to_s.titleize
+      det[:organisation] = User.find_by_email(data[1]).organisation if User.find_by_email(data[1]) && User.find_by_email(data[1]).organisation
+      det[:action] = "Logged in"
+      return User.find_by_email('joe@27stars.co.uk')
+    else
+      data = /The activity creation screen for (.*) was viewed/.match(self.message)
+      det[:user] = "Organisation Creator"
+      det[:level] = "Organisation Creator"
+      det[:organisation] = data[1].titleize
+      det[:action] = "Logged in"
+    end
+  end
 end
