646f4225bc92cd551bd86fcc9c82e08986107321
Joe Pym <joe@27stars.co.uk>
2009-07-27 14:52:20 +0100

    Added details for pdf log

--- app/models/pdf_log.rb
+++ app/models/pdf_log.rb
@@ -9,5 +9,21 @@
 class PDFLog < Log
   
   ICON = 'icons/pdf.gif'
-    
+  
+  def details
+    det = {}
+    data = /The (.*) manager PDF/.match(self.message)[1].gsub('"', '').gsub("'", '')
+    if data = 'activity'
+      activity = /<strong>(.*)<\/strong> activity/.match(self.message)[1]
+      organisation = /within <strong>(.*)<\/strong>/.match(self.message)[1]
+    else
+      organisation = /<strong>(.*)<\/strong>/.match(self.message)[1]
+    end
+    det[:user] = 'unknown'
+    det[:level] = data
+    det[:organisation] = organisation
+    det[:action] = "viewed pdf"
+    return det
+    end
+  end  
 end
