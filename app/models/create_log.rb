9dd6b524294e25cc7d5f621486ba47cf0259e0ff
Joe Pym <joe@27stars.co.uk>
2009-07-27 15:21:43 +0100

    Added creation logs

--- app/models/create_log.rb
+++ app/models/create_log.rb
@@ -9,5 +9,16 @@
 class CreateLog < Log
   
   ICON = 'icons/add.png'
+  
+  def details
+    det = {}
+    activity = /The <strong>(.*)<\/strong> activity/.match(self.message)[1].gsub('"', '').gsub("'", '')
+    organisation = /created for <strong>(.*)<\/strong>/.match(self.message)[1].gsub('"', '').gsub("'", '')
+    det[:user] = 'unknown'
+    det[:level] = 'organisation'
+    det[:organisation] = organisation
+    det[:action] = "#{activity} created."
+    return det
+  end
     
 end
