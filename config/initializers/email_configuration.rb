a034b6fc0eaa6723c302a23e2243d38c3bbcea35
Joe Pym <joe@27stars.co.uk>
2009-09-17 09:56:30 +0100

    new test

--- config/initializers/email_configuration.rb
+++ config/initializers/email_configuration.rb
@@ -2,7 +2,7 @@
 if RAILS_ENV != 'test'
   c = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
   ActionMailer::Base.smtp_settings = {
-    :address => c[RAILS_ENV]['server'],
+    :address => c[RAILS_ENV]['address'],
     :port => c[RAILS_ENV]['port'],
     :domain => c[RAILS_ENV]['domain'],
     :authentication => c[RAILS_ENV]['authentication'],
