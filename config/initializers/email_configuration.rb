c47455cde89091f446778309e6cbd358fe4ec3ae
Joe Pym <joe@27stars.co.uk>
2010-04-07 12:58:17 +0100

    Added gmail plugin, added configuration to use it

--- config/initializers/email_configuration.rb
+++ config/initializers/email_configuration.rb
@@ -1,15 +1,28 @@
 # Configuration for the SMTP server.
 if RAILS_ENV != 'test'
-  c = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
+  # c = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
+  # ActionMailer::Base.smtp_settings = {
+  #   :address => c[RAILS_ENV]['address'],
+  #   :port => c[RAILS_ENV]['port'],
+  #   :domain => c[RAILS_ENV]['domain'],
+  #   :pop3_auth => { 
+  #     :server => c[RAILS_ENV]['server'], 
+  #     :user_name => c[RAILS_ENV]['username'],
+  #     :password => c[RAILS_ENV]['password'],
+  #     :authentication => c[RAILS_ENV]['authentication']
+  #   }
+  # }
+  
+  
+  # No need with gmail for the email settings. Use hack from
+  # http://godbit.com/forum/viewtopic.php?id=876
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address => c[RAILS_ENV]['address'],
-    :port => c[RAILS_ENV]['port'],
-    :domain => c[RAILS_ENV]['domain'],
-    :pop3_auth => { 
-      :server => c[RAILS_ENV]['server'], 
-      :user_name => c[RAILS_ENV]['username'],
-      :password => c[RAILS_ENV]['password'],
-      :authentication => c[RAILS_ENV]['authentication']
-    }
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "blackradley.com",
+    :authentication => :plain,
+    :user_name => "support@blackradley.com",
+    :password => "impact2010"
   }
 end
