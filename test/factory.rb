2e11a28a6fbdfa56ed0c2842b1008d2b0ac3bfc3
Joe <joe@27stars.co.uk>
2011-02-22 17:22:40 +0000

    Added some activity tests

---
+++ test/factory.rb
@@ -0,0 +1,45 @@
+Factory.define :activity do |activity|
+  activity.sequence(:name){|i| "Activity #{i}"}
+  activity.association :activity_manager
+  activity.association :activity_approver
+  activity.association :directorate
+end
+
+Factory.define :user do |u|
+  u.sequence(:email){|i| "user#{i}@27stars.co.uk"}
+  u.passkey{|u| User.generate_passkey(u)}
+end
+
+Factory.define :activity_manager, :parent => :user, :class => ActivityManager do |am|
+  am.type "ActivityManager"
+end
+
+Factory.define :activity_approver, :parent => :user, :class => ActivityApprover do |am|
+  am.type "ActivityApprover"
+end
+
+Factory.define :organisation_manager, :parent => :user, :class => OrganisationManager do |om|
+  om.type "OrganisationManager"
+  om.association :organisation
+end
+
+Factory.define :directorate_manager, :parent => :user, :class => DirectorateManager do |dm|
+  dm.type "DirectorateManager"
+end
+
+Factory.define :directorate do |dir|
+  dir.sequence(:name){|i| "Directorate #{i}"}
+  dir.association :organisation
+  dir.association :directorate_manager
+end
+
+Factory.define :organisation do |org|
+  org.sequence(:name){|o| "Organisation #{o}"}
+  org.ces_term "Corporate Equality Scheme"
+  org.strategy_text_selection 0
+  org.subdomain "www"
+  
+  org.after_build do |o|
+     o.organisation_managers = (1..5).map{Factory.build(:organisation_manager, :organisation => o)}
+  end
+end
