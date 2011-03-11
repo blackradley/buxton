e4b721305bcff7363ae072f5dfaad5d143e7dc45
Joe Pym <joe@27stars.co.uk>
2011-03-11 17:55:12 +0000

    Added the autocomplete scope leve

---
+++ config/initializers/autocomplete.rb
@@ -0,0 +1,27 @@
+module Rails3JQueryAutocomplete
+  module Helpers
+    def get_autocomplete_items(parameters)
+      model = parameters[:model]
+      method = parameters[:method]
+      options = parameters[:options]
+      scope_level = options[:scope];
+      model = model.send(scope_level) if scope_level
+      term = parameters[:term]
+      is_full_search = options[:full]
+
+      limit = get_autocomplete_limit(options)
+      implementation = get_implementation(model)
+      order = get_autocomplete_order(implementation, method, options)
+
+      case implementation
+        when :mongoid
+          search = (is_full_search ? '.*' : '^') + term + '.*'
+          items = model.where(method.to_sym => /#{search}/i).limit(limit).order_by(order)
+        when :activerecord
+          items = model.where(["LOWER(#{method}) LIKE ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]) \
+            .limit(limit).order(order)
+      end
+    end
+
+  end
+end
