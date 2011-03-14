80b75ebe279881acf9a4ae1cc728e7bf9a418d6c
Joe Pym <joe@27stars.co.uk>
2011-03-14 11:13:19 +0000

    Fixed comments and notes with roles

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -9,10 +9,10 @@
 class CommentController < ApplicationController
   helper :sections
   layout false
-  before_filter :verify_question_access
+  before_filter :set_activity
 
   def set_comment
-    @parent_question = @current_user.activity.questions.find(params[:question_id])
+    @parent_question = @activity.questions.find(params[:question_id])
     unless @parent_question.comment then
       @parent_question.build_comment(:contents => params[:comment]).save!
     else
@@ -24,12 +24,12 @@ class CommentController < ApplicationController
   end
 
   def destroy
-    @parent_question = Question.find(params[:question_id])
+    @parent_question = @activity.questions.find(params[:question_id])
     @parent_question.comment.destroy if @parent_question && @parent_question.comment
   end
 
   def edit_strategy
-    @parent_strategy = @current_user.activity.activity_strategies.find(params[:activity_strategy_id])
+    @parent_strategy = @activity.activity_strategies.find(params[:activity_strategy_id])
     unless @parent_strategy.comment then
       @parent_strategy.build_comment(:contents => params[:comment]).save!
     else
@@ -38,24 +38,8 @@ class CommentController < ApplicationController
   end
   
   def destroy_strategy
-    @parent_strategy = ActivityStrategy.find(params[:activity_strategy_id])
+    @parent_strategy = @activity.activity_strategies.find(params[:activity_strategy_id])
     @parent_strategy.comment.destroy if @parent_strategy && @parent_strategy.comment
   end
-  
-  protected
-  
-    def get_related_model
-      Comment
-    end
-    
-    def verify_question_access
-      if params[:question_id]
-        activity = Question.find(params[:question_id]).activity
-      elsif params[:activity_strategy_id]
-        activity = ActivityStrategy.find(params[:activity_strategy_id]).activity
-      end
-      condition = ((activity && current_user.class.to_s.match(/^Activity/)) ? (current_user.activity == activity) : false)
-      verify_index_access get_related_model, nil, nil, false, condition
-    end
   
 end
