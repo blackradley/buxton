5303fcd5dda0634bcf905d305e7ec47a0d969b15
Shaun Keller <shaun@27stars.co.uk>
2011-03-14 10:52:19 +0000

    Mailer

---
+++ app/mailers/mailer.rb
@@ -0,0 +1,26 @@
+class Mailer < ActionMailer::Base
+  default :from => 'support@blackradley.com'
+  
+  default_url_options[:host] = case Rails.env
+                               when 'production'
+                                 'birmingham.impactengine.org.uk'
+                               when 'staging'
+                                 'birmingham.27stars.co.uk'
+                               else
+                                 'localhost:3000'
+                               end
+  
+  
+  def activity_created(activity)
+    @activity = activity
+    mail(:to => activity.completer.email,
+         :subject => "A new EINA has been created")
+    #mail completer
+  end
+  
+  def activity_completed(activity)
+    
+    #mail checker
+  end
+  
+end
