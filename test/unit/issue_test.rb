aa3f67c40aa4990c83a1e650a2c9e40d25d32ad7
Joe Pym <joe@27stars.co.uk>
2011-04-14 11:05:27 +0100

    Fixed issue tests

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -6,13 +6,32 @@
 #
 # Copyright (c) 2008 Black Radley Systems Limited. All rights reserved.
 #
+#
 require File.dirname(__FILE__) + '/../test_helper'
 
-class IssueTest < Test::Unit::TestCase
-  # fixtures :issues
+class IssueTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  context "with an issue that has been not completed" do
+    setup do
+      @activity = Factory.stub(:activity)
+      @issue = Factory(:issue, :activity => @activity, :strand => "gender", :section => "impact")
+    end
+    
+    should "not be complete" do
+      assert !@issue.check_responses
+    end
+    
+  end
+  
+  context "with an issue that been not completed" do
+    setup do
+      @activity = Factory.stub(:activity)
+      @issue = Factory(:issue, :activity => @activity, :actions => "Action", :timescales => "timescale", :lead_officer => "Joe", :strand => "gender", :section => "impact", :resources => "none")
+    end
+    
+    should "be complete" do
+      assert @issue.check_responses
+    end
+    
   end
 end
