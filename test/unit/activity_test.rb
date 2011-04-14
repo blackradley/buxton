9debd21ddb65886a418ce432016a22d0b4d8ac84
Joe Pym <joe@27stars.co.uk>
2011-04-14 15:19:50 +0100

    Added new activity tests

--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -62,4 +62,62 @@ class ActivityTest < ActiveSupport::TestCase
     end
     
   end
+  
+  
+  context "when an activity has started the initial assessment by answering the first section" do
+    setup do
+      @service_area = Factory(:service_area)
+      @completer = Factory(:user)
+      @approver = Factory(:user)
+      @activity = Factory(:activity, :completer => @completer, :approver => @approver, :service_area => @service_area)
+      @activity.questions.where(:name => ["purpose_overall_2", "purpose_overall_11", "purpose_overall_12"]).each do |q|
+        q.update_attributes(:raw_answer => "1")
+      end
+    end
+    
+    should "be started" do
+      assert @activity.started
+    end
+    
+    should "have the status of in the initial assessment" do
+      assert_equal "IA", @activity.progress
+    end
+    
+    should "not be completed" do
+      assert !@activity.completed
+    end
+    
+    should "have 1a completed" do
+      assert @activity.target_and_strategies_completed 
+    end
+    
+    should "not have 1b completed" do
+      assert !@activity.impact_on_individuals_completed
+    end
+    
+    should "not have 1c completed" do
+      assert !@activity.impact_on_equality_groups
+    end
+    
+    should "not have 1d completed" do
+      assert !@activity.questions.find_by_name('purpose_overall_13').completed?
+    end
+    
+    Activity.strands.each do |strand|
+      
+      should "not have impact #{strand} completed" do
+        assert !@activity.completed(:impact, strand.to_sym)
+      end
+      
+      should "not have consultation #{strand} completed" do
+        assert !@activity.completed(:consultation, strand.to_sym)
+      end
+      
+      should "not have additional work #{strand} completed" do
+        assert !@activity.completed(:additional_work, strand.to_sym)
+      end   
+      
+    end
+    
+  end
 end
