0b75a6a1054594af577527dcf9328f25b36b3964
Joe Pym <joe@27stars.co.uk>
2011-04-14 14:08:07 +0100

    Fixed the secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Buxton::Application.config.secret_token = '50826326f7253ab099348db9230f0bd80583bffd10570dde4f20cca333866c77c648d345de8b13f616c0da30bf27e2fcaea6c4d0cb08e6aaf6c07330066d7806'
+Buxton::Application.config.secret_token = '50826326f7253ab099348db9230f0bd80583bffd10570dde4f20cca333866c77c648d345de8b13f616c0da30bf27e2fcbea6c4d0cb08e6aaf6c07330066d7806'
