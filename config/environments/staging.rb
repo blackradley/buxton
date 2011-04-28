936fdab4341f25b2fade5cccbca7526fe807a9c0
Joe Pym <joe@27stars.co.uk>
2011-04-28 17:02:23 +0100

    Fixed email, fixed text of confirmation

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,7 +37,6 @@ Buxton::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'birmingham.27stars.co.uk' }
 end
 
 BANNER    = true
