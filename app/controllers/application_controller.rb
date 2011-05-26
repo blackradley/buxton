b419944a0b9ee7c46fc21b56ef374f4640d08428
Shaun Keller <shaun@27stars.co.uk>
2011-05-26 16:07:20 +0100

    Fixed styles for error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,7 +57,7 @@ protected
   end
   
   def not_found
-    render :file => "#{Rails.root}/public/404.html",  :status => "404 Not Found"
+    render :file => "#{Rails.root}/public/404.html",  :status => "404 Not Found", :layout => false
   end
   
   def requires_admin
