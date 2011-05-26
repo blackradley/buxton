a41e7a986228381497dc1bef063b7a022fb7174b
Shaun Keller <shaun@27stars.co.uk>
2011-05-26 16:07:20 +0100

    Fixed styles for error pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,60 +1,44 @@
 Buxton::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
-  # The production environment is meant for finished, "live" apps.
-  # Code is not reloaded between requests
   config.cache_classes = true
-
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
-  # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-  # If you have no front-end server that supports something like X-Sendfile,
-  # just comment this out and Rails will serve the files
-
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
-  
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
+  # Full error reports are disabled and caching is turned on
+  config.action_controller.consider_all_requests_local = false
+  config.action_controller.perform_caching             = true
+
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "blackradley.com",
-    :authentication => :plain,
-    :user_name => "support@blackradley.com",
-    :password => "impact2010"  
-  }
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # config.action_controller.asset_host                  = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable threaded mode
-  # config.threadsafe!
+  # Use the database for sessions instead of the file system
+  # (create the session table with 'rake db:sessions:create')
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
+  # We presently run on a .org.uk domain
+  TLD_LENGTH = 2
+
+  # Use sendmail protocol to deliver emails
+  config.action_mailer.delivery_method = :sendmail
+
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = false
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
-TLD_LENGTH = 2
+
+BANNER    = false
+KEYS      = false
+DEV_MODE  = BANNER
