52fa3e87097d93b645a8294978a8a300ee414a53
Shaun Keller <shaun@27stars.co.uk>
2011-05-26 14:39:10 +0100

    send mail in dev mode to developer only

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Buxton::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
-
+  config.action_mailer.delivery_method = :sendmail
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   config.action_mailer.default_url_options = { :host => 'buxton.dev' }
