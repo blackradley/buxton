4b2c40f4e345356b7590f59044f55d3bbb03f40f
Shaun Keller <shaun@27stars.co.uk>
2011-05-31 11:13:55 +0100

    Added preview environment

---
+++ config/deploy/preview.rb
@@ -0,0 +1,21 @@
+# =============================================================================
+# STAGING SPECIFIC VARIABLES
+# =============================================================================
+set :domain, "birmingham.27stars.co.uk"
+set :rails_env, "preview"
+set :port, 13427
+
+set :rake, 'rake'
+# =============================================================================
+# THIS WOULD BE IN DEPLOY.RB IF IT COULD BE LAZY EVALUATED, BUT IT CAN'T
+# SO IT MUST GO HERE :/ MAKE SURE STAGING.RB AND PRODUCTION.RB ARE THE SAME
+# =============================================================================
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+set :deploy_to, "/home/deploy/public_html/#{domain}"
+set :use_sudo, false
+
+ssh_options[:forward_agent] = true
+
+set :branch, 'rails3'
