cd3bf18037391e6123f8ba5b6370f0999fa052c1
Guy Wrigley <guy@27stars.co.uk>
2011-06-07 10:54:33 +0100

    tests for directorate controller

--- test/unit/directorate_test.rb
+++ test/unit/directorate_test.rb
@@ -8,11 +8,25 @@
 #
 require File.dirname(__FILE__) + '/../test_helper'
 
-class DirectorateTest < Test::Unit::TestCase
-  # fixtures :directorates
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
+# class DirectorateTest < ActionController::TestCase
+#   # fixtures :directorates
+# 
+#   context "with a creator" do
+#     setup do
+#       @creator = Factory(:user, :creator => true)
+#       sign_in @creator
+#     end
+#     
+#     should "be able to see a list of directorates" do
+#       get :index
+#       assert_response :success
+#     end
+#     
+#     should "be able to see the new directorates page" do
+#       get :new
+#       assert_response :success
+#     end
+#     
+#   end
+#   
+# end
