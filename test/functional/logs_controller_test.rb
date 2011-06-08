8918dd131306de536e0966af67b55af897d6c55b
Guy Wrigley <guy@27stars.co.uk>
2011-06-08 14:05:20 +0100

    Logs controller test added

---
+++ test/functional/logs_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class LogsControllerTest < ActionController::TestCase
+  
+  context "when not a cop" do
+    setup do
+      sign_in Factory(:user)
+    end
+    
+    should "not be able to access the logs" do
+      get :index
+      assert_response :redirect
+      assert_redirected_to access_denied_path
+    end
+    
+  end
+  
+  context "when anyone but a corporate cop" do
+    setup do
+      @user = Factory(:creator)
+      Factory(:activity, :completer => @user, :approver => @user, :qc_officer => @user)
+      sign_in @user
+    end
+    
+    should "not be able to access the logs" do
+      get :index
+      assert_response :redirect
+      assert_redirected_to access_denied_path
+    end
+    
+  end
+  
+  context "When an admin" do
+    setup do
+      sign_in Factory(:administrator)
+    end
+    
+    should "not be able to access the logs" do
+      get :index
+      assert_response :redirect
+      assert_redirected_to access_denied_path
+    end
+    
+  end
+    
+  context "When a corporate cop" do
+    setup do
+      sign_in Factory(:corporate_cop)
+    end
+    
+    should "be able to access the logs" do
+      get :index
+      assert_response :success
+    end
+    
+  end
+  
+end
