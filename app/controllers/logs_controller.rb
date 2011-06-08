a9859eee96b023b88e4eeb53d5b6f152f4b8b249
Guy Wrigley <guy@27stars.co.uk>
2011-06-08 12:20:35 +0100

    Removed unused clear and download methods from logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -16,22 +16,4 @@ class LogsController < ApplicationController
     @logs = Log.find(:all, :order => 'created_at DESC')
   end
   
-  def clear
-    Log.destroy_all
-    redirect_to :back
-  end
-  
-  def download
-    outfile = File.open('log/impact_equality_logs.csv', "wb") 
-    header_row = ["User",	"Organisation",	"Level", "Action", "Date", "Time"]	
-    CSV::Writer.generate(outfile) do |csv|
-      csv << header_row
-      Log.csv.each do |log|
-        csv << log
-      end
-    end
-    outfile.close
-    send_file 'log/impact_equality_logs.csv'
-  end
-
 end
