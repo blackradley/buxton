3a80f923b70160434099d7efd43fc5419605bd22
Lewis Wrigley <lewis@27stars.co.uk>
2011-12-21 15:36:19 +0000

    stuff

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,5 +11,15 @@ class PasswordsController < Devise::PasswordsController
       redirect_to access_denied_path
     end
   end
+
+  def create
+    self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+
+    if successfully_sent?(resource)
+      respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      respond_with_navigational(resource){ render_with_scope :new }
+    end
+  end
   
 end
