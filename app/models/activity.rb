2340a78229aac31cd25a3b349cca90348c84d62c
Joe Pym <joe@27stars.co.uk>
2012-05-15 16:16:56 +0100

    Added extra questions

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -674,15 +674,15 @@ class Activity < ActiveRecord::Base
                             :faith => [1,2,3,4,5,6,7],
                             :age => [1,2,3,4,5,6,7]
                           },    
-      :additional_work => { :race => [1,2,3,44,4,6],
-                            :disability => [1,2,3,44,4,6,7,8,9],
-                            :sexual_orientation => [1,2,3,44,4,6],
-                            :gender => [1,2,3,44,4],
-                            :gender_reassignment => [1,2,3,44,4,6],
-                            :pregnancy_and_maternity => [1,2,3,44,4,6],
-                            :marriage_civil_partnership => [1,2,3,44,4,6],
-                            :faith => [1,2,3,44,4,6],
-                            :age => [1,2,3,44,4,6]
+      :additional_work => { :race => [1,2,3,44,4,6, 10, 11],
+                            :disability => [1,2,3,44,4,6,7,8,9, 10, 11],
+                            :sexual_orientation => [1,2,3,44,4,6, 10, 11],
+                            :gender => [1,2,3,44,4, 10],
+                            :gender_reassignment => [1,2,3,44,4,6, 10, 11],
+                            :pregnancy_and_maternity => [1,2,3,44,4,6, 10, 11],
+                            :marriage_civil_partnership => [1,2,3,44,4,6, 10, 11],
+                            :faith => [1,2,3,44,4,6, 10, 11],
+                            :age => [1,2,3,44,4,6, 10, 11]
                           }
     }
   end
