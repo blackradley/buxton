7db57b66ef2dcf91266beef85467f1cfbaf1bd3a
Lewis <lewis@27stars.co.uk>
2012-08-20 16:25:40 +0100

    changed the csv generation thing to send data instead of a file

--- lib/schedule_csv_generator.rb
+++ lib/schedule_csv_generator.rb
@@ -15,14 +15,12 @@ class ScheduleCSVGenerator
   end
 
   def csv
-    path = 'tmp/schedule'
-    CSV.open(path, "wb") do |csv|
+    CSV.generate do |csv|
       csv << ["Directorate", "Service Area", "EA Reference", "EA Title", "Task Group Leader", "Senior Officer/Approver", "Quality Control Officer", "Status", "Progress", "Date Started", "Completion Date", "No. of Actions", "Action Plan Complete?"]
       @activities.each do |ac|
         csv << [ac.directorate.name, ac.service_area.name, ac.ref_no, ac.name, ac.completer.email, ac.approver.email, ac.qc_officer.email, ac.activity_status_name, ac.full_progress, ac.actual_start_date, ac.approved_on ? ac.approved_on.to_s(:full) : "", ac.relevant_action_count, ac.action_plan_completed ]
       end
     end
-    return path
   end
 
 end
