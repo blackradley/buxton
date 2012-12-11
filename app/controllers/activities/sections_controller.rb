c40d63a33bf121dc4472f0b8f5e629162ec3fb84
Lewis <lewis@27stars.co.uk>
2012-12-11 14:27:41 +0000

    fixed small permission problem

--- app/controllers/activities/sections_controller.rb
+++ app/controllers/activities/sections_controller.rb
@@ -138,7 +138,7 @@ class Activities::SectionsController < ApplicationController
   
   def set_activity
     @activity = current_user.activities.select{|a| a.id == params[:id].to_i}.first
-    if @activity || @activity.submitted
+    if @activity && @activity.submitted
       redirect_to access_denied_path
       return false
     end 
